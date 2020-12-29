576267efdadc80b5592ba250cac9c6a3d1b5da79
Neil Berg <neil.berg14@gmail.com>
2020-12-28 18:55:10 -0800

    custom servemux

--- main.go
+++ main.go
@@ -1,23 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/neil-berg/go-rest/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hello world")
-		d, err := ioutil.ReadAll(r.Body)
+	l := log.New(os.Stdout, "product-api ", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := http.Server{
+		Addr:         ":8000",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(w, "oops", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
-		fmt.Fprintf(w, "Hello %s", d)
-	})
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		fmt.Println("Bye world")
-	})
-	http.ListenAndServe(":8000", nil)
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Printf("Recieved terminal signal [%s], gracefully shutting down... \n", sig)
+
+	duration := time.Now().Add(30 * time.Second)
+	ctx, _ := context.WithDeadline(context.Background(), duration)
+	s.Shutdown(ctx)
 }
